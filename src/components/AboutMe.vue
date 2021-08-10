19552a8b8f9bc01a9a738c3f49d121b2c6c462c9
JasonTai <r041291867@gmail.com>
2021-08-10 15:50:00 +0800

    add english resume

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -18,11 +18,11 @@
         class="mx-auto title font-weight-light mb-8"
         max-width="720"
       >
-        I graduated from the Computer Science and Information Engineering of National Chiao Tung University. I enjoy helping others to solve their problems and get a sense of accomplishment from it. I often admire the various graphic and web designs, and I hope that I can become a developer with a sense of beauty and design ability in addition to being an engineer.
+        I'm a front-end engineer with back-end experience. I enjoy helping others to solve their problems and get a sense of accomplishment from it. I often admire the various graphic and web designs, and I hope that I can become a developer with a sense of beauty and design ability in addition to being an engineer.
       </v-responsive>
 
       <v-avatar
-        class="elevation-12 mb-12"
+        class="elevation-12 mb-8"
         size="128"
       >
         <v-img src="@/assets/IMG_5194.jpeg"></v-img>
@@ -30,29 +30,49 @@
 
       <div></div>
 
-      <v-btn
-        color="grey"
-        href="https://www.cakeresume.com/s--SnneDe0JAgKLZX5U5n-UIg--/r041291867"
-        outlined
-        large
+      <div
+        class="grey--text text--darken-2 font-weight-bold text-uppercase btn-class mb-1"
+        text
       >
-        <span class="grey--text text--darken-1 font-weight-bold">
-          Resume
-        </span>
-      </v-btn>
+        Resume
+      </div>
+      <v-responsive
+        class="mx-auto mb-2"
+        width="56"
+      >
+        <v-divider></v-divider>
+      </v-responsive>
 
-     <!--  <v-btn
-        color="grey"
-        href="https://www.cakeresume.com/pdf/s--iS2UODGN-Rc9ODENa5mjug--/EDDmJ.pdf"
-        large
-        icon
+      <resume-button
+        href="https://www.cakeresume.com/s--SnneDe0JAgKLZX5U5n-UIg--/r041291867"
       >
-        <v-icon>
-          mdi-download
-        </v-icon>
-      </v-btn> -->
+        Chinese
+      </resume-button>
+
+      <resume-button
+        href="https://www.canva.com/design/DAEjlwa-4H4/view?utm_content=DAEjlwa-4H4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+      >
+        English
+      </resume-button>
     </v-container>
 
     <div class="py-12"></div>
   </section>
 </template>
+
+<script>
+  export default {
+    components: {
+      ResumeButton: () => import('./Button'),
+    }
+  }
+</script>
+
+<style scoped>
+  .btn-class {
+    font: Roboto;
+    font-size: 1rem;
+    font-weight: 500;
+    letter-spacing: .0892857143em;
+  }
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn
+    color="teal"
+    :href="$attrs.href"
+    text
+  >
+    <span class="teal--text text--darken-2 font-weight-bold">
+      <slot />
+    </span>
+  </v-btn>
+</template>
